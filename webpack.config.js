d4ee939583fe89c3794442b2261000964ff6b3b6
fanyong <fanyong@gmail.com>
2015-11-29 16:49:42 +0800

    stage2

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ process.env.REACT_WEBPACK_ENV = env;
 // Get available configurations
 var configs = {
   base: require(path.join(__dirname, 'cfg/base')),
-  dev: require(path.join(__dirname, 'cfg/dev')),
+  dev:  require(path.join(__dirname, 'cfg/dev')),
   dist: require(path.join(__dirname, 'cfg/dist')),
   test: require(path.join(__dirname, 'cfg/test'))
 };
